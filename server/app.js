56c3d7b05268022048bae855ba4ca28e0d918c5f
Zeeshan Ahmad <za5743759@gmail.com>
2026-01-21 01:25:55 +0500

    add frontned url to backend cors

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use("/payment/webhook", express.raw({ type: "application/json" }), stripeWebHook); // for stripe webhook
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "https://bookora-z.vercel.app/"], credentials: true }));
 app.use(express.json());
 app.use(
   session({
